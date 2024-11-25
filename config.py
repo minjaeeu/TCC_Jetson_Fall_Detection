217b4c0ace20ad82dbcf2a8b3b023e1311d929f2
minjaeeu <minjaeeu@outlook.com>
2024-11-24 21:09:36 -0400

    config template file

---
+++ config.py
@@ -0,0 +1,10 @@
+# e-mail configuriton parameters
+SMTP_SERVER = ""
+SMTP_PORT = 
+EMAIL_USER = ""
+EMAIL_PASSWORD = ""
+EMAIL_RECIPIENTS = []
+
+# WhatsApp configution parameters (has to be a business account for it to work)
+WHATSAPP_API_URL = ""
+WHATSAPP_API_KEY = ""
