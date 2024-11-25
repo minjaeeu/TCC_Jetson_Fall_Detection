82e3c112509e21bfa1d866a8ce04f25faa81f21b
minjaeeu <minjaeeu@outlook.com>
2024-11-24 20:49:23 -0400

    .gitignore updated to ignore config file

--- config.py
+++
@@ -1,9 +0,0 @@
-# e-mail configuriton parameters
-SMTP_SERVER = ""
-SMTP_PORT = ""
-EMAIL_USER = ""
-EMAIL_PASSWORD = ""
-
-# WhatsApp configution parameters (has to be a business account for it to work)
-WHATSAPP_API_URL = ""
-WHATSAPP_API_KEY = ""
